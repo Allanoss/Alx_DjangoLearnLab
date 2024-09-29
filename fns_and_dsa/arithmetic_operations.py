4ce0de14dc4adf18c9b1d3497d55f408d65a3f4b
Allano <alllan.ooko@gmail.com>
2024-09-29 12:10:28 +0300

    Fixing Div by 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,6 @@ def perform_operation(num1, num2, operation):
         if num2 != 0:
             return num1/num2
         else:
-            print("Div by 0 not possible")
+            return "Div by 0 not possible"
     else:
         print("Please check the values you provided or the operation")
