1b83e11cf0d00b0df1d66ee7c9944adadf875f4d
Allano <alllan.ooko@gmail.com>
2024-11-17 15:20:18 +0300

    SecTry

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,32 @@
+# LibraryProject/bookshelf/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import BookSearchForm, CustomUserRegistrationForm
+from .models import Book
+
+def search_books(request):
+    if request.method == 'POST':
+        form = BookSearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            books = Book.objects.filter(title__icontains=query)
+            return render(request, 'bookshelf/book_list.html', {'books': books})
+    else:
+        form = BookSearchForm()
+
+    return render(request, 'bookshelf/search_books.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserRegistrationForm()
+
+    return render(request, 'bookshelf/register.html', {'form': form})
