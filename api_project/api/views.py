86c49815344dcf8620ce82a2c1ba3a8388651222
Allano <alllan.ooko@gmail.com>
2024-11-24 22:59:10 +0300

    TestingCRUD

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
-class BookList(generics.ListAPIView):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on the Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
